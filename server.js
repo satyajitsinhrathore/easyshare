45649c39c1cf507ad17e62aa1fa9ff9034facb1d
Satyajitsinh Rathore <satyajitrtr22@gmail.com>
2022-11-06 17:46:59 +0530

    add cors

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const cors = require('cors');
 
 
 const PORT = process.env.PORT || 3000;
@@ -8,6 +9,13 @@ app.use(express.static('public'))
 app.use(express.json());
 const connectDB = require('./config/db');
 connectDB();
+
+// cors
+const corsOptions = {
+    origin: process.env.ALLOWED_CLIENTS.split(',')
+}
+
+app.use(cors(corsOptions));
 
 // template engine
 app.set('views', path.join(__dirname, '/views'));
